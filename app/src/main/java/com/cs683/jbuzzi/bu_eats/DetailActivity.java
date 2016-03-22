aaa87e8c3b437448f7dd0f30852f98b91fc4086b
jorgebuzzi <jbuzzi@mobiquityinc.com>
2016-03-22 04:31:33 -0400

    Project setup
    Implemented app mealtime greeting.

---
+++ app/src/main/java/com/cs683/jbuzzi/bu_eats/DetailActivity.java
@@ -0,0 +1,13 @@
+package com.cs683.jbuzzi.bu_eats;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
---
+++ app/src/main/java/com/cs683/jbuzzi/bu_eats/MainActivity.java
@@ -0,0 +1,162 @@
+package com.cs683.jbuzzi.bu_eats;
+
+import android.net.Uri;
+import android.os.Bundle;;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import java.util.Date;
+import java.util.Calendar;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.text.ParseException;
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    private GoogleApiClient client;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        //Displays app greeting based on time of day
+        TextView header = (TextView) findViewById(R.id.header);
+        try {
+            if (isTimeBetween("05:00:00", "10:59:59")){
+                header.setText("It's breakfast time!");
+                Log.v(TAG, "showing breakfast gretting");
+            } else if (isTimeBetween("11:00:00", "16:59:59")){
+                header.setText("It's lunch time!");
+                Log.v(TAG, "showing lunch greeting");
+            } else if (isTimeBetween("17:00:00", "20:59:59")){
+                header.setText("It's dinner time!");
+                Log.v(TAG, "showing dinner greeting");
+            } else if (isTimeBetween("21:00:00", "04:59:59")){
+                header.setText("Looking for a late night snack?");
+                Log.v(TAG, "showing late night greeting");
+            } else {
+                header.setText("Hungry?");
+                Log.v(TAG, "showing default greeting");
+            }
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.cs683.jbuzzi.bu_eats/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.cs683.jbuzzi.bu_eats/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }
+
+    //Returns true if current time is between initialTime and finalTime. Use to determined the time
+    //of day.
+    boolean isTimeBetween(String initialTime, String finalTime) throws ParseException {
+        String reg = "^([0-1][0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9])$";
+        if (initialTime.matches(reg) && finalTime.matches(reg)) {
+            boolean valid = false;
+            //Start Time
+            Date inTime = new SimpleDateFormat("HH:mm:ss").parse(initialTime);
+            Calendar calendar1 = Calendar.getInstance();
+            calendar1.setTime(inTime);
+
+            //Current Time
+            DateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+            Date date = new Date();
+            String currentTime = dateFormat.format(date);
+            Date checkTime = new SimpleDateFormat("HH:mm:ss").parse(currentTime);
+            Calendar calendar3 = Calendar.getInstance();
+            calendar3.setTime(checkTime);
+
+            //End Time
+            java.util.Date finTime = new SimpleDateFormat("HH:mm:ss").parse(finalTime);
+            Calendar calendar2 = Calendar.getInstance();
+            calendar2.setTime(finTime);
+
+            if (finalTime.compareTo(initialTime) < 0) {
+                calendar2.add(Calendar.DATE, 1);
+                calendar3.add(Calendar.DATE, 1);
+            }
+
+            java.util.Date actualTime = calendar3.getTime();
+            if ((actualTime.after(calendar1.getTime()) || actualTime.compareTo(calendar1.getTime()) == 0)
+                    && actualTime.before(calendar2.getTime())) {
+                valid = true;
+            }
+            return valid;
+        } else {
+            throw new IllegalArgumentException("Not a valid time, expecting HH:MM:SS format");
+        }
+    }
+}
